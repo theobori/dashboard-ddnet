c5a3a1bb0a11fbb7f975f3447e488cd7cc3e2dfb
Théo Bori <theobori@disroot.org>
2026-05-10 18:56:11 +0200

    Add DDNet player general informations insert
    Also add docstrings and fix last activity

--- dashboard-ddnet.el
+++ dashboard-ddnet.el
@@ -45,6 +45,10 @@
  'dashboard-item-generators
  '(ddnet-player-last-activity . dashboard-ddnet--insert-player-last-activity))
 
+(add-to-list
+ 'dashboard-item-generators
+ '(ddnet-player-general-informations . dashboard-ddnet--insert-player-general-informations))
+
 (defgroup dashboard-ddnet ()
   "Display DDNet player informations on Dashboard"
   :group 'tools
@@ -52,9 +56,9 @@
 
 ;;;; User options and variables
 
-(defvar dashboard-ddnet--cache nil "")
-(defvar dashboard-ddnet--cache-float-time 0 "")
-(defvar dashboard-ddnet--cache-ttl 15 "")
+(defvar dashboard-ddnet--cache-data nil "HTTP request reponse data cached.")
+(defvar dashboard-ddnet--cache-float-time 0 "The current time as a float of seconds since the epoch.")
+(defvar dashboard-ddnet--cache-ttl 300 "Time to live for the cached data.")
 
 (defcustom dashboard-ddnet-player-name "nameless tee"
   "DDNet player name"
@@ -69,7 +73,7 @@
 ;;;; Functions and Emacs user commands
 
 (defun assocdr (key alist &optional testfn)
-  ""
+  "Wrapping the `assoc' function then returning its CDR."
   (cdr (assoc key alist testfn)))
 
 (defun dashboard-ddnet--unix-timestamp-format (timestamp &optional zone)
@@ -77,63 +81,69 @@
   (format-time-string "%Y-%m-%d %H:%M" timestamp (or zone "UTC")))
 
 (defun dashboard-ddnet--json-player-url-format ()
-  ""
+  "Returns the DDNet URL to get player informations as JSON."
   (url-encode-url
    (format "%s/players/?json2=%s"
 	   dashboard-ddnet-url dashboard-ddnet-player-name)))
 
 (defun dashboard-ddnet--page-player-url-format (&optional player-name)
-  ""
+  "Returns the DDNet URL to get a player page."
   (url-encode-url
    (format "%s/players/%s"
 	   dashboard-ddnet-url (or player-name
 				   dashboard-ddnet-player-name))))
 
 (defun dashboard-ddnet--page-map-url-format (map-name)
-  ""
+  "Returns the DDNet URL to get a map page."
   (url-encode-url
    (format "%s/maps/%s"
 	   dashboard-ddnet-url map-name)))
 
 (defun dashboard-ddnet--get-json-player-informations ()
-  ""
+  "Returns the player informations as a Emacs value mirroring JSON. If the
+cached data has no more time to live, it makes HTTP request and updates
+the cache values."
   (if (<= (- (float-time) dashboard-ddnet--cache-float-time) dashboard-ddnet--cache-ttl)
-      dashboard-ddnet--cache
-    (setq dashboard-ddnet--cache (request-response-data
-				  (request (dashboard-ddnet--json-player-url-format)
-				    :parser 'json-read
-				    :sync t)))
+      dashboard-ddnet--cache-data
+    (setq dashboard-ddnet--cache-data (request-response-data
+				       (request (dashboard-ddnet--json-player-url-format)
+					 :parser 'json-read
+					 :sync t)))
     (setq dashboard-ddnet--cache-float-time (float-time))
-    dashboard-ddnet--cache))
+    dashboard-ddnet--cache-data))
 
-(defun dashboard-ddnet--insert-pairs-helper (title pairs)
-  ""
-  (when pairs
+(defun dashboard-ddnet--insert-alist-helper (title alist)
+  "Helper function that first insert a TITLE, then ALIST where each cons
+should have the inserted value as car and an associated url as cdr."
+  (when alist
     (dashboard-insert-heading title)
-    (dolist (pair pairs)
+    (dolist (el alist)
       (insert "\n    ")
       (widget-create 'push-button
-                     :action `(lambda (&rest ignore)
-				(browse-url ,(cdr pair)))
+                     :action `(lambda (&rest _)
+				(browse-url ,(cdr el)))
                      :mouse-face 'highlight
                      :button-face 'dashboard-items-face
                      :follow-link "\C-m"
                      :button-prefix ""
                      :button-suffix ""
                      :format "%[%t%]"
-		     (car pair)))))
+		     (car el)))))
 
 (defun dashboard-ddnet--insert-player-vector-helper (title player-information-key vector-parser-function list-size)
-  ""
+  "Helper function that get specific player informations that must be a
+vector with a given PLAYER-INFORMATION-KEY. Apply VECTOR-PARSER-FUNCTION
+on it to get a alist and finally insert each element of the alist by
+calling `dashboard-ddnet--insert-alist-helper'."
   (let* ((player-informations (dashboard-ddnet--get-json-player-informations))
 	 (vector (seq-take
 		  (assocdr player-information-key player-informations)
 		  list-size))
-	 (pairs (funcall vector-parser-function vector)))
-    (dashboard-ddnet--insert-pairs-helper title pairs)))
+	 (alist (funcall vector-parser-function vector)))
+    (dashboard-ddnet--insert-alist-helper title alist)))
 
 (defun dashboard-ddnet--player-last-finish-format (last-finish)
-  ""
+  "Returns a formatted string representing a LAST-FINISH alist."
   (let ((time (floor (assocdr 'time last-finish)))
 	(timestamp (assocdr 'timestamp last-finish)))
     (format "%s: %s %s: %s (%02d:%02d)"
@@ -145,7 +155,7 @@
 	    (% time 60))))
 
 (defun dashboard-ddnet--player-last-finishes-parse (last-finishes)
-  "list of cons desired format 08/05/2026 23:36: GER Moderate: Jvice (12:34)"
+  "Parses the LAST-FINISHES vector and returns a alist."
   (let (ans)
     (dotimes (i (length last-finishes))
       (let* ((last-finish (aref last-finishes i))
@@ -157,22 +167,22 @@
     (reverse ans)))
 
 (defun dashboard-ddnet--insert-player-last-finishes (list-size)
-  ""
+  "Insert the DDNet player LIST-SIZE last finishes."
   (when (> list-size 10)
-    (error "It cannot fetch more than 10 last finishes"))
+    (error "It cannot fetch more than 10 last finishes."))
   (dashboard-ddnet--insert-player-vector-helper "DDNet last finishes:"
 						'last_finishes
 						#'dashboard-ddnet--player-last-finishes-parse
 						list-size))
 
 (defun dashboard-ddnet--player-favorite-partner-format (favorite-partner)
-  ""
+  "Returns a formatted string representing a FAVORITE-PARTNER alist."
   (format "%s: %d ranks"
 	  (assocdr 'name favorite-partner)
 	  (assocdr 'finishes favorite-partner)))
 
 (defun dashboard-ddnet--player-favorite-partners-parse (favorite-partners)
-  ""
+  "Parses the FAVORITE-PARTNERS vector and returns a alist."
   (let (ans)
     (dotimes (i (length favorite-partners))
       (let ((favorite-partner (aref favorite-partners i)))
@@ -184,16 +194,16 @@
     (reverse ans)))
 
 (defun dashboard-ddnet--insert-player-favorite-partners (list-size)
-  ""
+  "Insert the DDNet player LIST-SIZE favorite partners."
   (when (> list-size 10)
-    (error "It cannot fetch more than 10 favorite partners"))
+    (error "It cannot fetch more than 10 favorite partners."))
   (dashboard-ddnet--insert-player-vector-helper "DDNet favorite partners:"
 						'favorite_partners
 						#'dashboard-ddnet--player-favorite-partners-parse
 						list-size))
 
 (defun dashboard-ddnet--player-last-activity-parse (activity)
-  ""
+  "Parses the LAST-ACTIVITY vector and returns a alist."
   (let ((ans)
 	(activity-desc (reverse activity)))
     (dotimes (i (length activity-desc))
@@ -206,11 +216,42 @@
     (reverse ans)))
 
 (defun dashboard-ddnet--insert-player-last-activity (list-size)
-  ""
-  (dashboard-ddnet--insert-player-vector-helper "DDNet last activity:"
-						'activity
-						#'dashboard-ddnet--player-last-activity-parse
-						list-size))
+  "Insert the DDNet player LIST-SIZE last activity."
+  (let* ((player-informations (dashboard-ddnet--get-json-player-informations))
+	 (last-activity-vector (seq-take
+				(reverse (assocdr 'activity player-informations))
+				list-size))
+	 (last-activity-alist
+	  (dashboard-ddnet--player-last-activity-parse last-activity-vector)))
+    (dashboard-ddnet--insert-alist-helper "DDNet last activity:"
+					  last-activity-alist)))
+
+(defun dashboard-ddnet--insert-player-general-informations (&optional list-size)
+  "Insert the DDNet player general informations. LIST-SIZE will not be used
+for this section."
+  (let ((player-informations (dashboard-ddnet--get-json-player-informations))
+	(action-function `(lambda (&rest _)
+			    (browse-url ,(dashboard-ddnet--json-player-url-format)))))
+    (dashboard-insert-heading "DDNet player general informations:")
+    (insert "\n    ")
+    (widget-create 'push-button
+                   :action action-function
+		   :mouse-face 'highlight
+		   :button-face 'dashboard-items-face
+		   :follow-link "\C-m"
+		   :button-prefix ""
+		   :button-suffix ""
+		   :format "Player: %[%t%]"
+		   (assocdr 'player player-informations))
+    (insert "\n    ")
+    (let ((points (assocdr 'points player-informations)))
+      (insert (format "Rank: %d" (assocdr 'rank points)))
+      (insert "\n    ")
+      (insert (format "Points: %d" (assocdr 'points points))))
+    (insert "\n    ")
+    (insert (format "Favorite server: %s"
+		    (assocdr 'server
+			     (assocdr 'favorite_server player-informations))))))
 
 ;;; Code
 (provide 'dashboard-ddnet)
